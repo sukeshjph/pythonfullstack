962f39400da6c2682a4e4d25869c594d92db650e
Dash, Sukesh <UI617823@rwe.com>
2025-08-04 10:39:58 +0100

    Book change

--- financial-app/backend/books.py
+++ financial-app/backend/books.py
@@ -3,21 +3,23 @@ import json
 
 app = FastAPI()
 
+def getBooksFromjson():
+    with open("books.json", "r") as file:
+        data = json.load(file)
+    return data["books"]
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/books")
 async def get_books():
-    with open("books.json", "r") as file:
-        data = json.load(file)
-    return data
+    books = getBooksFromjson()
+    return books
 
 @app.get("/books/{book_id}")
 async def get_book_by_id(book_id: int):
-    with open('books.json', 'r') as file:
-        data = json.load(file)
-    books = data["books"]
+    books = getBooksFromjson()
     for book in books:
         if book['id'] == book_id:
             return book
@@ -25,9 +27,7 @@ async def get_book_by_id(book_id: int):
 
 @app.get("/books/")
 async def get_book_by_genre_Query(genre: str):
-    with open('books.json', 'r') as file:
-        data = json.load(file)
-    books = data["books"]
+    books = getBooksFromjson()
     books_to_return = []
     for book in books:
         if book['genre'].casefold() == genre.casefold():
@@ -36,35 +36,32 @@ async def get_book_by_genre_Query(genre: str):
 
 @app.post("/books/create_book")
 async def create_book(new_book: dict = Body()):
-    with open('books.json', 'r') as file:
-        data = json.load(file)
-    
-    books = data["books"]
-    
+    books = getBooksFromjson()
     # Generate a new ID (assuming IDs are sequential)
     new_id = max(book['id'] for book in books) +1    
     new_book['id'] = new_id
     
     books.append(new_book)
-    data["books"] = books
-    
+
     with open('books.json', 'w') as file:
-        json.dump(data, file, indent=4)
+        json.dump(books, file, indent=4)
     
     return {"message": "Book created successfully", "book": new_book}
 
 @app.put("/books/update_book")
 async def update_book(updated_book=Body()):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
-            BOOKS[i] = updated_book
+    books = getBooksFromjson()
+    for i in range(len(books)):
+        if books[i].get('title').casefold() == updated_book.get('title').casefold():
+            books[i] = updated_book
 
 
 @app.delete("/books/delete_book/{book_title}")
 async def delete_book(book_title: str):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
-            BOOKS.pop(i)
+    books = getBooksFromjson()
+    for i in range(len(books)):
+        if books[i].get('title').casefold() == book_title.casefold():
+            books.pop(i)
             break
     
 
